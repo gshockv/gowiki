da593f407c4775375718cf7dd9a7a92585a1ea53
Vitaly Gashock <gshockv@gmail.com>
2014-01-10 11:52:52 +0200

    small renaming

--- wiki.go
+++ wiki.go
@@ -33,12 +33,12 @@ var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
+	t := validPath.FindStringSubmatch(r.URL.Path)
+	if t == nil {
 		http.NotFound(w, r)
 		return "", errors.New("Invalid Page Title")
 	}
-	return m[2], nil
+	return t[2], nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
